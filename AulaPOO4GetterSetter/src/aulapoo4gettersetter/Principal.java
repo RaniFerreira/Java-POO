45e3cefb42b065f4c3627624624da09616cd4fc7
RaniFerreira <franielly535@gmail.com>
2024-09-30 21:01:56 -0300

    add

---
+++ AulaPOO4GetterSetter/src/aulapoo4gettersetter/Principal.java
@@ -0,0 +1,25 @@
+package aulapoo4gettersetter;
+
+public class Principal {
+    public static void main(String[] args) {
+        Carro fusca = new Carro();
+        
+        fusca.setMarca("wolkswagen");
+        fusca.setModelo("Fusca");
+        fusca.setAno(1982);
+        
+        System.out.println("Carro: " + fusca.getMarca());
+        System.out.println("Ano: " + fusca.getAno());
+    }
+    
+   
+    
+   
+    
+    
+    
+   
+    
+    
+    
+}
